d6117f709565cdc0951d31464c15c548f66e54d2
Valentino Gagliardi <valentino@valentinog.com>
2017-12-27 12:12:45 +0100

    Use combineReducers

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,16 +1,4 @@
-import { ADD_ARTICLE } from "../constants/action-types";
+import { combineReducers } from "redux";
+import articleReducer from "./articleReducer";
 
-const initialState = {
-  articles: []
-};
-
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_ARTICLE:
-      return { ...state, articles: [...state.articles, action.payload] };
-    default:
-      return state;
-  }
-};
-
-export default rootReducer;
+export default combineReducers({ articles: articleReducer });

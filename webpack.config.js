a911b5dc4b4e6420f0c9f5b9d06622b5b6b177d0
Valentino Gagliardi <valentino@valentinog.com>
2018-03-11 15:19:34 +0100

    Upgrade to webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,6 @@
-const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ["./src/js/index.js"],
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/[name].js"
-  },
-  devServer: {
-    contentBase: "./dist"
-  },
   module: {
     rules: [
       {

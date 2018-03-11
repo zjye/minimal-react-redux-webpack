4513af752977c9b7505bb216c4d545072e737d7d
Valentino Gagliardi <valentino@valentinog.com>
2018-03-11 15:00:59 +0100

    Fixed a typo

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import store from "../js/store/index";
-import App from "../js/components/App";
+import store from "./store/index";
+import App from "./components/App";
 
 render(
   <Provider store={store}>
